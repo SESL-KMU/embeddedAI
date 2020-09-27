a502317d6821e6a36804afdf3dc04d0f274810be
SESL-KMU <71960101+SESL-KMU@users.noreply.github.com>
2020-09-27 19:18:17 +0900

    Add files via upload

---
+++ CNN_example/mnist_example_test_tflite.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import tflite_runtime.interpreter as tflite
+from matplotlib import pyplot as plt
+import time
+
+interpreter = tflite.Interpreter(model_path="./converted_model.tflite")
+interpreter.allocate_tensors()
+
+image = cv2.imread('img.png', cv2.IMREAD_GRAYSCALE)
+image = image / 255.0
+test_image = np.expand_dims(image, axis=0)
+
+ans = input('do you wanna check running time? (y/n)')
+start = time.perf_counter()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+input_data = np.array(test_image.reshape(1, 28, 28), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
+if ans =='y' or ans =='Y':
+    elapsed = time.perf_counter() - start
+    print('Elapsed %.3f seconds.' % elapsed)
+
+plt.imshow(test_image[0], cmap='Greys')
+plt.show()
+
