ca0d8b68ed3aac929dcb56a5e0c04e8b8027f00c
SESL-KMU <71960101+SESL-KMU@users.noreply.github.com>
2020-09-28 21:29:28 +0900

    Update stock_valid.py

--- RNN_example/stock_valid.py
+++ RNN_example/stock_valid.py
@@ -1,29 +1,52 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import tflite_runtime.interpreter as tflite
+from matplotlib import pyplot as plt
 import time
 
 ans = input('do you wanna check running time? (y/n)')
 start = time.perf_counter()
 
-df = pd.read_csv('B.csv')
-df = df[df['Close'].notnull()]
-print(df.head())
-df_close = df[['Close']]
-df_close.index = df['Date']
-print(df_close)
-df_close.plot(subplots=True)
-if ans == 'n' or ans == 'N':
-    plt.show()
+fb = open('./B.txt', 'r')
+data = ()
+date = ()
+while True:
+    line = fb.readline()
+    if not line:
+        break
+    if line[0] is 'D':
+        continue
+    line = np.array(line.split(','))
+    line = np.delete(line, np.where(line == ''))
+    Close = float(line[4])
+    Date = line[0]
+    data = np.append(data, Close)
+    date = np.append(date, Date)
 
-stock_data = df_close.values
+print(data)
+
+stock_data = data[:, np.newaxis]
 stock_data_min = stock_data.min()
-stock_data_max_min_sub = stock_data.max()-stock_data.min()
-stock_data = (stock_data-stock_data_min)/stock_data_max_min_sub
+stock_data_max_min_sub = stock_data.max() - stock_data.min()
+stock_data = (stock_data - stock_data_min) / stock_data_max_min_sub
 
 past_history = 365
 future_target = 30
+
+interpreter = tflite.Interpreter(model_path="./converted_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+input_data = np.array(np.array([stock_data[-past_history:]]), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
+
 
 def create_time_steps(length):
     time_steps = []
@@ -31,27 +54,28 @@ def create_time_steps(length):
         time_steps.append(i)
     return time_steps
 
+
 def multi_step_plot(history, true_future, prediction, title):
-  plt.figure(figsize=(12, 6))
-  num_in = create_time_steps(len(history))
-  num_out = len(true_future)
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
 
-  plt.title(title)
+    plt.title(title)
 
-  plt.plot(num_in, np.array(history), label='History')
-  plt.plot(np.arange(num_out), np.array(true_future), label='True Future')
-  if prediction.any():
-    plt.plot(np.arange(len(prediction)), np.array(prediction), label='Predicted Future')
-  plt.legend(loc='upper left')
-  if ans == 'n' or ans == 'N':
-      plt.show()
+    plt.plot(num_in, np.array(history), label='History')
+    plt.plot(np.arange(num_out), np.array(true_future), label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(len(prediction)), np.array(prediction), label='Predicted Future')
+    plt.legend(loc='upper left')
+    if ans == 'n' or ans == 'N':
+        plt.show()
 
-simple_lstm_model = tf.keras.models.load_model('stock_rnn', compile = False)
 
-TRAIN_SPLIT = int(len(stock_data)*0.7)
+TRAIN_SPLIT = int(len(stock_data) * 0.7)
+
 
 def data_geenrator(dataset, start_index, end_index, history_size, target_size, single_step=False):
-    data = []
+    _data_ = []
     labels = []
 
     start_index = start_index + history_size
@@ -60,27 +84,32 @@ def data_geenrator(dataset, start_index, end_index, history_size, target_size, s
 
     for i in range(start_index, end_index):
 
-        indices = range(i-history_size, i)
+        indices = range(i - history_size, i)
         # Reshape data from (history_size,) to (history_size, 1)
-        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        _data_.append(np.reshape(dataset[indices], (history_size, 1)))
         if single_step:
             labels.append(dataset[i + target_size])
         else:
             labels.append(dataset[i:i + target_size])
-    return np.array(data), np.array(labels).squeeze()
+    return np.array(_data_), np.array(labels).squeeze()
+
 
 x_val, y_val = data_geenrator(stock_data, TRAIN_SPLIT, None,
-                                       past_history,
-                                       future_target)
+                              past_history,
+                              future_target)
 
-BATCH_SIZE = 32
 
-val_tensor = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_tensor = val_tensor.batch(BATCH_SIZE).repeat()
+if ans == 'n' or ans == 'N':
 
-for x, y in val_tensor.take(1):
-    plot = multi_step_plot(x[0].numpy(), y[0].numpy(),
-                           simple_lstm_model.predict(x)[0], 'Validation Predict')
+    input_shape_val = input_details[0]['shape']
+    input_data_val = np.array(np.array(x_val[:, np.newaxis][0]), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data_val)
+
+    interpreter.invoke()
+
+    output_data_val = interpreter.get_tensor(output_details[0]['index'])
+    multi_step_plot(x_val[0], y_val[0],
+                    output_data_val[0], 'Validation Predict')
 
 if ans == 'y' or ans == 'Y':
     elapsed = time.perf_counter() - start
