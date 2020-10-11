133f357b323be9e52ef1562e4cda3c577d694dea
SESL-KMU <71960101+SESL-KMU@users.noreply.github.com>
2020-10-11 23:03:12 +0900

    time check
    change

--- CNN_example/mnist_example_test_tflite.py
+++ CNN_example/mnist_example_test_tflite.py
@@ -12,7 +12,6 @@ image = image / 255.0
 test_image = np.expand_dims(image, axis=0)
 
 ans = input('do you wanna check running time? (y/n)')
-start = time.perf_counter()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
@@ -21,14 +20,18 @@ input_shape = input_details[0]['shape']
 input_data = np.array(test_image.reshape(1, 28, 28), dtype=np.float32)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
-interpreter.invoke()
-
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
-
 if ans =='y' or ans =='Y':
-    elapsed = time.perf_counter() - start
-    print('Elapsed %.3f seconds.' % elapsed)
+    for _ in range(3):
+        start = time.perf_counter()
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        print(output_data)
+        elapsed = time.perf_counter() - start
+        print('Elapsed %.3f seconds.' % elapsed)
+else:
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(output_data)
 
 plt.imshow(test_image[0], cmap='Greys')
 plt.savefig('savefig_default.png')
